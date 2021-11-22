7c30306f138b666a0c810c9cb612e033303a2042
zzlin1003 <zzl.ustc@hotmail.com>
2021-11-21 21:36:05 -0500

    redirect to TEDVAE IHDP dataset

--- datasets.py
+++ datasets.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 
 
 class IHDP(object):
-    def __init__(self, path_data="datasets/IHDP/setting_a", replications=10):
+    def __init__(self, path_data="../TEDVAE/IHDP", replications=10):
         self.path_data = path_data
         self.replications = replications
         # which features are binary
@@ -13,7 +13,8 @@ class IHDP(object):
 
     def __iter__(self):
         for i in range(self.replications):
-            data = np.loadtxt(self.path_data + '/ihdp_npci_' + str(i + 1) + '.csv', delimiter=',')
+            data = np.loadtxt(self.path_data + '/ihdp_npci_train_' + str(i + 1) + '.csv', 
+                delimiter=',',skiprows=1)
             t, y, y_cf = data[:, 0], data[:, 1][:, np.newaxis], data[:, 2][:, np.newaxis]
             mu_0, mu_1, x = data[:, 3][:, np.newaxis], data[:, 4][:, np.newaxis], data[:, 5:]
             yield (x, t, y), (y_cf, mu_0, mu_1)
